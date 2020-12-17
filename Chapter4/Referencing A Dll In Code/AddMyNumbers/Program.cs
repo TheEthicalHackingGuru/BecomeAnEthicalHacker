af35f35afe0104a34b4d8aed00f6ad3bd3edef65
TheEthicalHackingGuru <75399456+TheEthicalHackingGuru@users.noreply.github.com>
2020-12-17 07:49:39 -0600

    Create Program.cs

---
+++ Chapter4/Referencing A Dll In Code/AddMyNumbers/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using MyFirstClassLibrary;
+
+
+namespace AddMyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers ad = new AddNumbers();
+            int newNumber = ad.AddTwoNumbers(1, 2);
+            Console.WriteLine(newNumber);
+        }
+    }
+}
