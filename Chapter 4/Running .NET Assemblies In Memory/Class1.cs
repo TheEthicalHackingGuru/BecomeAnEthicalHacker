5b7484872229d18855cb668cd351340f6feb4c0c
TheEthicalHackingGuru <75399456+TheEthicalHackingGuru@users.noreply.github.com>
2021-02-02 19:57:34 -0600

    Create Class1.cs

---
+++ Chapter 4/Running .NET Assemblies In Memory/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace RunDllPublicFunc
+{
+    public class Class1
+    {
+
+    public static void PrintMessage()
+        {
+            Console.WriteLine("Hello from inside the dll!");
+        }
+    }
+}
